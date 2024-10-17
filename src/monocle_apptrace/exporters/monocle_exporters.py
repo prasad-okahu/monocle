2c54df39d01a09151f4a41f9db0bd10b8b24821b
Prasad Mujumdar <prasad@okahu.ai>
2024-10-17 16:57:29 -0700

    Okahu and log exporter
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/exporters/monocle_exporters.py
+++ src/monocle_apptrace/exporters/monocle_exporters.py
@@ -1,29 +1,25 @@
 from typing import Dict
 import os
 from opentelemetry.sdk.trace.export import SpanExporter
-import exporters
-import exporters.aws
-import exporters.aws.s3_exporter
-import exporters.azure
-import exporters.azure.blob_exporter
-import exporters.file_exporter
-import exporters.log_exporter
-import exporters.okahu
-import exporters.okahu.okahu_exporter
+from monocle_apptrace.exporters.aws.s3_exporter import S3SpanExporter
+from monocle_apptrace.exporters.azure.blob_exporter import AzureBlobSpanExporter
+from monocle_apptrace.exporters.file_exporter import FileSpanExporter
+from monocle_apptrace.exporters.log_exporter import LogSpanExporter
+from monocle_apptrace.exporters.okahu.okahu_exporter import OkahuSpanExporter
 
 monocle_exporters:Dict[str, SpanExporter.__class__] = [
-    {"s3": exporters.aws.s3_exporter.S3SpanExporter},
-    {"blob": exporters.azure.blob_exporter.AzureBlobSpanExporter},
-    {"okahu": exporters.okahu.okahu_exporter.OkahuSpanExporter},
-    {"file": exporters.file_exporter.FileSpanExporter},
-    {"log": exporters.log_exporter.LogSpanExporter}
+    {"s3": S3SpanExporter},
+    {"blob": AzureBlobSpanExporter},
+    {"okahu": OkahuSpanExporter},
+    {"file": FileSpanExporter},
+    {"log": LogSpanExporter}
 ]
 
 def get_monocle_exporter() -> SpanExporter:
     exporter_name = os.environ.get("MONOCLE_EXPORTER", "file")
-    exporter_class = monocle_exporters.get(exporter_name, exporters.file_exporter.FileSpanExporter)
+    exporter_class = monocle_exporters.get(exporter_name, FileSpanExporter)
     try:
         return exporter_class()
     except Exception as ex:
         print(ex)
-        return exporters.log_exporter.LogSpanExporter()
+        return LogSpanExporter()
