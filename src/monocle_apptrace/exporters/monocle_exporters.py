d3884de2bc179af7da7a75d60e377e4e99093f79
Prasad Mujumdar <prasad@okahu.ai>
2024-10-17 18:09:12 -0700

    Okahu and log exporter
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/exporters/monocle_exporters.py
+++ src/monocle_apptrace/exporters/monocle_exporters.py
@@ -3,6 +3,7 @@ import os
 from importlib import import_module
 from opentelemetry.sdk.trace.export import SpanExporter
 from monocle_apptrace.exporters.log_exporter import LogSpanExporter
+from monocle_apptrace.exporters.file_exporter import FileSpanExporter
 
 monocle_exporters:Dict[str, Any] = [
     {"s3": {"module": "monocle_apptrace.exporters.aws.s3_exporter", "class": "S3SpanExporter"}},
@@ -14,7 +15,11 @@ monocle_exporters:Dict[str, Any] = [
 
 def get_monocle_exporter() -> SpanExporter:
     exporter_name = os.environ.get("MONOCLE_EXPORTER", "file")
-    exporter_class_path  = monocle_exporters.get(exporter_name, monocle_exporters.get("file"))
+    try:
+        exporter_class_path  = monocle_exporters[exporter_name]
+    except Exception as ex:
+        print(ex)
+        return FileSpanExporter()
     exporter_module = import_module(exporter_class_path.get("module"))
     exporter_class = getattr(exporter_module, exporter_class_path.get("class"))
     try:
