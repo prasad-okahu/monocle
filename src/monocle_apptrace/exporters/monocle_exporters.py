c208a67434daffb97c91b8bc4410bd7618970f2f
Prasad Mujumdar <prasad@okahu.ai>
2024-10-17 17:50:29 -0700

    Okahu and log exporter
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/exporters/monocle_exporters.py
+++ src/monocle_apptrace/exporters/monocle_exporters.py
@@ -1,23 +1,22 @@
-from typing import Dict
+from typing import Dict, Any
 import os
+from importlib import import_module
 from opentelemetry.sdk.trace.export import SpanExporter
-from monocle_apptrace.exporters.aws.s3_exporter import S3SpanExporter
-from monocle_apptrace.exporters.azure.blob_exporter import AzureBlobSpanExporter
-from monocle_apptrace.exporters.file_exporter import FileSpanExporter
 from monocle_apptrace.exporters.log_exporter import LogSpanExporter
-from monocle_apptrace.exporters.okahu.okahu_exporter import OkahuSpanExporter
 
-monocle_exporters:Dict[str, SpanExporter.__class__] = [
-    {"s3": S3SpanExporter},
-    {"blob": AzureBlobSpanExporter},
-    {"okahu": OkahuSpanExporter},
-    {"file": FileSpanExporter},
-    {"log": LogSpanExporter}
+monocle_exporters:Dict[str, Any] = [
+    {"s3": {"module": "monocle_apptrace.exporters.aws.s3_exporter", "class": "S3SpanExporter"}},
+    {"blob": {"module":"monocle_apptrace.exporters.azure.blob_exporter", "class": "AzureBlobSpanExporter"}},
+    {"okahu": {"module":"monocle_apptrace.exporters.okahu.okahu_exporter", "class": "OkahuSpanExporter"}},
+    {"file:": {"module":"monocle_apptrace.exporters.file_exporter", "class": "FileSpanExporter"}},
+    {"log:": {"module":"monocle_apptrace.exporters.log_exporter", "class": "LogSpanExporter"}}
 ]
 
 def get_monocle_exporter() -> SpanExporter:
     exporter_name = os.environ.get("MONOCLE_EXPORTER", "file")
-    exporter_class = monocle_exporters.get(exporter_name, FileSpanExporter)
+    exporter_class_path  = monocle_exporters.get(exporter_name, monocle_exporters.get("file"))
+    exporter_module = import_module(exporter_class_path.get("module"))
+    exporter_class = getattr(exporter_module, exporter_class_path.get("class"))
     try:
         return exporter_class()
     except Exception as ex:
