79d72c71bb15492a38b53035992dc016e0a8721c
Prasad Mujumdar <prasad@okahu.ai>
2025-01-14 18:49:39 -0800

    Multi msg trace with http instrumentation
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

---
+++ src/monocle_apptrace/instrumentation/metamodel/requests/methods.py
@@ -0,0 +1,16 @@
+from monocle_apptrace.instrumentation.common.wrapper import task_wrapper
+
+REQUESTS_METHODS = [
+    {
+        "package": "requests.sessions",
+        "object": "Session",
+        "method": "request",
+        "span_name": "http_requests",
+        "wrapper_method": task_wrapper,
+        "pre_processor": {
+            "module": "monocle_apptrace.instrumentation.metamodel.requests._helper",
+            "method": "request_pre_processor"
+        },
+        "skip_span": True
+    }
+]
