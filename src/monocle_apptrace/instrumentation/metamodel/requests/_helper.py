95274de7d1baad4df1388dd542d5ad571302eada
Prasad Mujumdar <prasad@okahu.ai>
2025-02-06 19:48:43 -0800

    Allowlist for request instrumentation
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/instrumentation/metamodel/requests/_helper.py
+++ src/monocle_apptrace/instrumentation/metamodel/requests/_helper.py
@@ -1,10 +1,18 @@
+import os
+from  monocle_apptrace.instrumentation.metamodel.requests import allowed_urls
 from opentelemetry.propagate import inject
+
 def request_pre_processor(args, kwargs):
     # add traceparent to the request headers in kwargs
-    if 'headers' not in kwargs:
-        headers = {}
-    else:
-        headers = kwargs['headers']
-    inject(headers)
-    kwargs['headers'] = headers
+    if 'url' in kwargs:
+        url:str = kwargs['url']
+        for allowed_url in allowed_urls:
+            if url.startswith(allowed_url):
+                if 'headers' not in kwargs:
+                    headers = {}
+                else:
+                    headers = kwargs['headers']
+                inject(headers)
+                kwargs['headers'] = headers
+                return
 
